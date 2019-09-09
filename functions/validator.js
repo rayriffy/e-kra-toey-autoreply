686f7b1b471d54781f965d12a5eb1925a1c389d9
Phumrapee Limpianchop <git@rayriffy.com>
2019-09-09 18:04:53 +0700

    :recycle: Refactor

--- functions/validator.js
+++ functions/validator.js
@@ -7,10 +7,6 @@ const validator = text => {
   const resValid = text.match(validRegex)
   const resInvalid = text.match(invalidRegex)
 
-  console.log('>')
-  console.log(JSON.stringify(resValid))
-  console.log(JSON.stringify(resInvalid))
-
   return !_.isEmpty(resValid) && _.isEmpty(resInvalid)
 }
 
